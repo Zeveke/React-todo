adf0797e3c6fef921b538da7f9bf8296b170cd39
Zeveke <143583225+Zeveke@users.noreply.github.com>
2023-12-15 23:31:39 +0400

    Delete directory

--- hooks/index.js
+++
@@ -1,5 +0,0 @@
-export * from './use-request-add-the-note';
-export * from './use-request-advanced-filter';
-export * from './use-request-completed-item';
-export * from './use-request-delete-item';
-export * from './use-request-handle-search';
