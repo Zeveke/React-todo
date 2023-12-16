bcc7a9bd70d15c19901de7d84d30fcec47552bdd
Zeveke <143583225+Zeveke@users.noreply.github.com>
2023-12-16 13:31:11 +0400

    add hooks

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,5 +1,3 @@
-export * from './use-request-add-the-note';
-export * from './use-request-advanced-filter';
-export * from './use-request-completed-item';
-export * from './use-request-delete-item';
-export * from './use-request-handle-search';
+import './use-add-todos';
+import './use-todos-search';
+import './use-todos-sort';
